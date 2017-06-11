07239580d212cdb867f3a865f4e62d34ecd4be21
Kyle Krattiger <mr.music25@gmail.com>
2017-06-11 14:05:36 -0700

    well, that was easier than expected. Moving on to Lab05_02

--- COMP 110/Lab05_01.java
+++ COMP 110/Lab05_01.java
@@ -1,3 +1,26 @@
+import java.util.Scanner;
 public class Lab05_01 {
-
+   public static void main(String args[]) {
+      // Initialize variables
+      String s1 = "NULL", s2 = "NULL"; // Didn't compile last time with no value
+      int index;
+      
+      // Ask for input
+      Scanner input1 = new Scanner(System.in);
+      System.out.printf("Enter string s1: ");
+      s1 = input1.nextLine();
+      
+      Scanner input2 = new Scanner(System.in);
+      System.out.printf("Enter string s2: ");
+      s2 = input2.nextLine();
+      
+      // Now compare strings
+      index = s1.indexOf(s2);
+      
+      // Print output based on results
+      if ( index == -1 )
+         System.out.println(s2 + " is not a substring of " + s1);
+      else  
+         System.out.println(s2 + " is a substring of " + s1 + ", starting at index " + index);   
+   }
 }
