af14799709c8cda8e8841199dbc4b33a2450feb1
Kyle Krattiger <mr.music25@gmail.com>
2017-06-13 09:12:10 -0700

    Started work on today's lab

---
+++ COMP 110/Lab07_01.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Lab07_01 {
+   public static void main(String args[]) {
+      // Input the number to check
+      int num;
+      
+      Scanner input = new Scanner(System.in);
+      System.out.printf("Please enter an integer: ");
+      num = input.nextInt();
+   }
+}
