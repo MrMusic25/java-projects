cc3e8312cc8cb47a06dc3cc5b11f5b1061a93634
Kyle Krattiger <mr.music25@gmail.com>
2017-06-15 08:30:33 -0700

    .... 9 lines..... The midterm ended up being 9... lines.... Wow...

--- COMP 110/MidTerm.java
+++ COMP 110/MidTerm.java
@@ -1,5 +1,9 @@
 public class MidTerm {
    public static void main(String args[]) {
-   
+      double result = 0.0;
+      for (int i = 1; i <= 624; i++) {
+         result += 1 / (Math.sqrt(i) + Math.sqrt(i + 1));
+      }
+      System.out.println("Value is: " + result);   
    }
 }
