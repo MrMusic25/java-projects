ffc42b610344b972a9180419a9cab2833cc2e927
Kyle Krattiger <mr.music25@gmail.com>
2017-06-19 23:06:23 -0700

    Forgot to commit this

--- COMP 110/Lab08_02.java
+++ COMP 110/Lab08_02.java
@@ -1,5 +1,21 @@
 public class Lab08_02 {
    public static void main(String args[]) {
+      // No input, just output the given table
+      System.out.println("i                                    m(i)  ");
+      System.out.println("-------------------------------------------");
+      System.out.println(1 + "                                    " + findPi(1)); // Needed a custom line
+      
+      for (int x = 101; x <= 901; x += 100) {
+      System.out.println(x + "                                  " + findPi(x));
+      }
+   }
    
+   public static double findPi(int i) {
+      // Find pi up until integer given
+      double sum = 0.0;
+      for (int j = i; j != 0; j--) {
+         sum += (Math.pow(-1,j+1)) / (2 * j - 1);
+      }
+      return 4.0 * sum;
    }
 }
