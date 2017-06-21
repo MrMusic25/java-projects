493a01d6c6d44ca9c413719f8fbebcfda91c8f70
Kyle Krattiger <mr.music25@gmail.com>
2017-06-21 00:33:16 -0700

    Never forget to commit changes, children, even if it's all for fun

--- COMP 110/Lab08_02.java
+++ COMP 110/Lab08_02.java
@@ -5,15 +5,15 @@ public class Lab08_02 {
       System.out.println("-------------------------------------------");
       System.out.println(1 + "                                    " + findPi(1)); // Needed a custom line
       
-      for (int x = 101; x <= (int)(Math.pow(2,32) / 2 - 1); x += 100) {
+      for (long x = 100000; x <= (long)(Math.pow(2,63) / 2 - 1); x += 100000) {
       System.out.println(x + "                                  " + findPi(x));
       }
    }
    
-   public static double findPi(int i) {
+   public static double findPi(long i) {
       // Find pi up until integer given
       double sum = 0.0;
-      for (int j = i; j != 0; j--) {
+      for (long j = i; j != 0; j--) {
          sum += (Math.pow(-1,j+1)) / (2 * j - 1);
       }
       return 4.0 * sum;
