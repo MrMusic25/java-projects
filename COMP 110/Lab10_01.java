77e839b843cc4ccd0da984df881e45d3eeef1ceb
Kyle Krattiger <mr.music25@gmail.com>
2017-06-21 09:45:47 -0700

    Tried to finish in class, but I got hungry...

--- COMP 110/Lab10_01.java
+++ COMP 110/Lab10_01.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
 public class Lab10_01 {
    public static void main(String args[]) {
+      // Input number of students
+      Scanner numIn = new Scanner(System.in);
+      System.out.printf("Enter the number of students: ");
+      int numStudents = numIn.nextInt();
+      int studentGrades[] = new int[numStudents];
+      
+      // Now get grades
+      Scanner gradesIn = new Scanner(System.in);
+      System.out.printf("Enter " + numStudents + " grades: ");
+      for (int i = 0; i < studentGrades.length; i++)
+         studentGrades[i] = gradesIn.nextInt();
+      printGrades(studentGrades);   
+   }
    
+   public static void printGrades(int grades[]) {
+      int best = grades[findHighestValue(grades)];
+      
+      for (int j = 0; j < grades.length; j++) {
+         
+      }
+   }
+   
+   public static int findHighestIndex(int a[]) {
+      int index = 0;
+      for (int x = 0; x < a.length; x++)
+         if (a[x] > a[index])
+            index = x;
+      return index;      
    }
 }
