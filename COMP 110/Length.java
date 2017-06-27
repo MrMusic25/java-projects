9033ddbd9942832c522c411b97458af22f913b49
Kyle Krattiger <mr.music25@gmail.com>
2017-06-26 18:55:32 -0700

    I think it's ready. Now to make a control program and test it out!

--- COMP 110/Length.java
+++ COMP 110/Length.java
@@ -40,9 +40,42 @@ public class Length {
    public Length add(Length otherLength) {
       int tmpFeet = this.feet + otherLength.getFeet();
       int tmpInches = this.inches + otherLength.getInches();
-      Length newLength;
-      newLength = new Length(tmpFeet, tmpInches);
+      Length newLength = new Length();
+      /* newLength = new Length(tmpFeet, tmpInches);
+         ^-- Above line kept giving me compiler error, couldn't find overloaded method
+      */
       
+      newLength.setFeet(tmpFeet);
+      newLength.setInches(tmpInches); // Method automatically takes care of >=12 cases
       return newLength;
    }
+   
+   public Length subtract(Length otherLength) {
+      Length newLength = new Length();
+      if (this.feet <= otherLength.getFeet()) { // -ge because of below code --v
+         if (!(this.feet - otherLength.getFeet() == 0 && this.inches >= otherLength.getInches())) { // This allows for 0,0 and inches-only calc
+            newLength.setFeet(-1);
+            newLength.setInches(-1); // Indicates error, cannot subtract
+            return newLength;
+         }   
+      }
+      
+      // Above error checking should cover all use cases
+      // Subtraction can be assumed to be successful now, moving on!
+      int tmpFeet = this.feet - otherLength.getFeet();
+      int tmpInches = this.inches;
+      if (this.inches < otherLength.getInches()) {
+         tmpFeet--;
+         tmpInches += 12;
+      }
+      tmpInches -= otherLength.getInches();
+      newLength.setFeet(tmpFeet);
+      newLength.setInches(tmpInches);
+      return newLength;   
+   }
+   
+   public String toString() {
+      String output = this.feet + "' " + this.inches + "\"";
+      return output;
+   }   
 }
