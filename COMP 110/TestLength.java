c9e9582a8f17cb39d8cb7492a65b729400bf3c65
Kyle Krattiger <mr.music25@gmail.com>
2017-06-26 21:00:10 -0700

    Still not working. Committing before I re-write the whole thing.

--- COMP 110/TestLength.java
+++ COMP 110/TestLength.java
@@ -4,29 +4,30 @@ public class TestLength {
       // Everything can be done with one for loop
       Length lengths[] = new Length[4];
       for (int i = 0; i < lengths.length /* lol */; i++) {
-         lengths[i] = new Length();
          switch (i) {
             case 0:
+               lengths[i] = new Length();
                lengths[i].setFeet(10);
                lengths[i].setInches(5);
                break;
             case 1:
+               lengths[i] = new Length();
                lengths[i].setFeet(5);
                lengths[i].setInches(10);
                outputLength(i-1,lengths[i-1]);
                outputLength(i,lengths[i]);
                break;
             case 2:
-               lengths[i].setFeet(lengths[0].getFeet());
-               lengths[i].setInches(lengths[0].getInches());
-               lengths[i].add(lengths[1]);
-               outputLength(i,lengths[i]);
+               //lengths[i].setFeet(lengths[0].getFeet());
+               //lengths[i].setInches(lengths[0].getInches());
+               Length lengths2 = lengths[0].add(lengths[1]);
+               outputLength(i,lengths2);
                break;
             case 3:
-               lengths[i].setFeet(lengths[0].getFeet());
-               lengths[i].setInches(lengths[0].getInches());
-               lengths[i].subtract(lengths[1]);
-               outputLength(i,lengths[i]);
+               //lengths[i].setFeet(lengths[0].getFeet());
+               //lengths[i].setInches(lengths[0].getInches());
+               Length lengths3 = lengths[1].subtract(lengths[0]);
+               outputLength(i,lengths3);
          }
       }
 
