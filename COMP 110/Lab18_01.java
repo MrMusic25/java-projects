6db4e2341ea21e700e3a5e291bcc0f48fc76eb2c
Kyle Krattiger <mr.music25@gmail.com>
2017-07-05 08:29:07 -0700

    Writer's block. committing while I think.

---
+++ COMP 110/Lab18_01.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Lab18_01 {
+   public static void main(String args[]) {
+      
+   }
+   
+   public static int count(String str, char e) {
+      
+   }
+}
