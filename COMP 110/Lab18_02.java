d7c0f41e5c44cd6f8adc15ed369f4a2a8c48e8d1
Kyle Krattiger <mr.music25@gmail.com>
2017-07-05 09:09:17 -0700

    Never thought I would find recursion useful, but look at me now!

--- COMP 110/Lab18_02.java
+++ COMP 110/Lab18_02.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
 public class Lab18_02 {
    public static void main(String args[]) {
+      // Have user input interger
+      Scanner input = new Scanner(System.in);
+      System.out.printf("Please enter an interger: ");
+      long n = input.nextLong();
+      
+      System.out.println("Sum of digits of " + n + " is: " + sumDigits(n));
+   }
    
+   public static int sumDigits(long n) {
+      if ( n <= 0 )
+         return 0;
+      int divisor = 1; // This allows it to return a single digit
+      while (n / divisor > 0)
+         divisor *= 10; // This will get us the highest divisor that yields a digit
+         
+      int x = (int)(n / (divisor/10));
+      return x + sumDigits(n-(x*(divisor/10)));      
    }
 }
