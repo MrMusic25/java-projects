7a5028065b87c20143cc1b09a221cad1e7fff108
Kyle Krattiger <mr.music25@gmail.com>
2017-07-05 08:48:12 -0700

    Finished Lab18_01, moving on to Lab18_02

--- COMP 110/Lab18_01.java
+++ COMP 110/Lab18_01.java
@@ -1,10 +1,28 @@
 import java.util.Scanner;
 public class Lab18_01 {
    public static void main(String args[]) {
+      // User inputs a string, then the char to be counted
+      Scanner strIn = new Scanner(System.in);
+      System.out.printf("Input a string: ");
+      String str = strIn.nextLine();
       
+      Scanner charIn = new Scanner(System.in);
+      System.out.printf("Input a char to be counted: ");
+      char e = charIn.nextLine().charAt(0);
+      
+      System.out.println(e + " occurs " + count(str,e,str.length()-1) + " times in the given string.");
    }
    
-   public static int count(String str, char e) {
-      
+   public static int count(String str, char e, int pos) {
+      if ( pos < 0 )
+         return 0;
+      return charEquals(str.charAt(pos),e) + count(str,e,pos-1); // Thought this would create an infinite loop, but it didn't.... Cool.
+   }
+   
+   public static int charEquals(char a, char b) {
+      if ( a == b )
+         return 1;
+      else
+         return 0;   
    }
 }
---
+++ COMP 110/Lab18_02.java
@@ -0,0 +1,5 @@
+public class Lab18_02 {
+   public static void main(String args[]) {
+   
+   }
+}
