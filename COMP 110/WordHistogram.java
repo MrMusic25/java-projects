3667077060cb1e7aa8aa0b3631fb12bad7524f0a
Kyle Krattiger <mr.music25@gmail.com>
2017-07-10 08:38:22 -0700

    Finished debugging, project is done!

--- COMP 110/WordHistogram.java
+++ COMP 110/WordHistogram.java
@@ -21,18 +21,20 @@ public class WordHistogram {
       StringBuilder input = new StringBuilder();
       Scanner inFileReader = new Scanner(inFile);
       while (inFileReader.hasNext()) {
-         input.append(inFileReader.nextLine());
+         input.append(inFileReader.nextLine() + "\n");
       }
       inFileReader.close();
       
       // Now, convert all non-alphabet chars to '\n'
       int newlineCount = 0;
       for (int i = 0; i < input.length(); i++) {
-         if ( (input.charAt(i) >= 'a' && input.charAt(i) <= 'z') ||
-              (input.charAt(i) >= 'A' && input.charAt(i) <= 'Z') ) {
+         if ( !(input.charAt(i) >= 'a' && input.charAt(i) <= 'z') &&
+              !(input.charAt(i) >= 'A' && input.charAt(i) <= 'Z') ) {
               input.setCharAt(i,'\n');
               newlineCount++;
-         }     
+         }
+         //else
+           // input.setCharAt(i,input.charAt(i).toLowerCase());     
       }        
       String processedInput = input.toString();
       
@@ -51,10 +53,12 @@ public class WordHistogram {
    
    // Returns location of word if it exists, -1 if it was not found
    public static int wordExists(String database[], String word) {
-      if (database.length <= 0) return -1;
+      //if (database.length <= 0) return -1;
       for (int a = 0; a < database.length; a++)
-         if (database[a] == word)
+         if (database[a].equals(word))
             return a;
+         else if (database[a].equals(""))
+            return -1;   
       return -1;      
    }
    
@@ -65,6 +69,7 @@ public class WordHistogram {
       
       while (input.hasNext()) {
          word = input.next();
+         word = word.toLowerCase();
          rvalue = wordExists(database,word);
          if (rvalue == -1) {
             int location = nextOpenSpot(database);
@@ -89,7 +94,7 @@ public class WordHistogram {
       
       while (database[index] != "" && index < database.length) {
          if (database[index].length() == max)
-            output.append(" " + database[index] + " : " + wordCount[index] + "\r\n");
+            output.append("  " + database[index] + " : " + wordCount[index] + "\r\n");
          else {
             spaces = max - database[index].length();
             for (int i = 0; i <= spaces; i++)
