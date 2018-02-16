e287b1a2ac0d10594ebc73d893330c9752ef5b44
Kyle Krattiger <mr.music25@gmail.com>
2018-02-16 10:15:49 -0800

    Uploading work on in-class project about sorting

---
+++ COMP 182/Sorting Practice/ArraySort.java
@@ -0,0 +1,26 @@
+/*
+   Name: Kyle Krattiger
+   
+   Function: An abstrac class to be used for sorting algorithm classes
+   
+*/
+public abstract class ArraySort {
+	public String name; // Name of the sorting algorithm
+	public String input; // The original input from the user or program
+	
+	// Constructors
+	public ArraySort() {
+		name = "Unknown";
+		input = "";
+	}
+	
+	public ArraySort(String s) {
+		name = "Unknown";
+		input = s;
+	}
+	
+	// Abstract methods
+	public abstract String toString(); // As name implies, return string of the sorted object
+	public abstract void sortIncr(); // Sort from low->high
+	public abstract void sortDecr(); // Sort from high->low
+}
---
+++ COMP 182/Sorting Practice/Bubble.java
@@ -0,0 +1,27 @@
+/* 
+   Name: Kyle Krattiger
+   
+   Function: A class that utilizes the bubble sort algorithm
+*/
+public class Bubble extends ArraySort {
+	// Constructors
+	public Bubble() {
+		name = "Bubble Sort";
+		input = "";
+	}
+	
+	public Bubble(String s) {
+		name = "Bubble Sort";
+		input = s;
+	}
+	
+	// Completed abstract methods
+	public String toString() {
+		
+	}
+	
+	public void sortIncr() {
+		
+	}
+	// Additional methods
+}
