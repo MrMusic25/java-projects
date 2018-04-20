3b90407b7a6a731a5420c4ca8d356e6ad7541fb6
Kyle Krattiger <mr.music25@gmail.com>
2018-04-20 09:02:11 -0700

    Until I start writing the other parts of the program, Song.java is ready. Compiles successfully.

--- m3u2USB/M3U2USB.java
+++ m3u2USB/M3U2USB.java
@@ -1,6 +1,11 @@
 import java.util.Scanner;
 
 public class M3U2USB {
+	/*
+        Global Variables
+	*/
+	public boolean copyDRM = true; // How to handle songs with DRM. true, just copy them over; false, skip the song and notify user
+	
 	public static void main(String args[]) {
 		// Using this program assume you have a music folder with the following structure:
 		// ../Music/<Artist>/<Album>/<Song>
--- m3u2USB/Song.java
+++ m3u2USB/Song.java
@@ -5,8 +5,8 @@ public class Song {
 	/*
 		Class Attributes
 	*/
-	protected String fullInputPath, fullOutputPath;
-	protected String artist, album, title, extension;
+	protected String fullInputPath, fullOutputPath, outputFolder;
+	protected String artist, album, title, extension, parent, origExt;
 	
 	/*
 		Valid output modes are as follows
@@ -23,6 +23,10 @@ public class Song {
 	private boolean convertSong = true; // Leave true to convert song (by default), false to copy
 	private Log sLog;
 	
+	// This will work for now, make this list modifiable after release
+	public static String allowedExt[] = new String[]{ "mp3" }; // Extensions that can be copied instead of converted, saving time
+	public static String unconvertable[] = new String[]{ "m4p" }; // Songs that will throw an error because of DRM protection, making them unplayable
+	
 	/*
 		Constructors
 	*/
@@ -33,7 +37,10 @@ public class Song {
 		this.outputMode = 'x';
 		this.artist = null;
 		this.album = null;
+		this.parent = null;
 		this.title = null;
+		this.outputFolder = null;
+		this.origExt = null;
 		this.sLog = null;
 	}
 	
@@ -42,16 +49,21 @@ public class Song {
 		this.sLog = new Log(l,false); // Assuming shared logfile, do not reinitialize
 		
 		this.fullInputPath = inFile;
-		this.seperatePath();
+		this.outputFolder = outFolder;
 		
 		// Determine folder divider char
 		if (this.fullInputPath.charAt(0) == '/')
-			this.divider = '/'; // Windows implicit, Linux/MacOS/BSD explicit. Conversion is later, so nbd
+			this.swapDivider();
+		this.seperatePath();
 		
-		// Check if extensions match
-		if (this.extension == ext) 
+		// Check if extensions match allowed, or unconvetable extension types
+		if (this.extension == ext || contains(ext,allowedExt)) 
 			this.convertSong = false;
-		else
+		else if (contains(ext,unconvertable)) {
+            sLog.log("Song " + fullInputPath + " is unconvertable!",3);
+            this.convertSong = false;
+        }
+        else
 			this.extension = ext;
 		
 		// Delete numbers if user specified
@@ -63,9 +75,9 @@ public class Song {
 				else
 					continue;
 		}
-			
+		
 		this.outputMode = mode;
-		this.fullOutputPath = determineOutput(outFolder);
+		this.fullOutputPath = determineOutput(this.outputFolder);
 	}
 	
 	/*
@@ -76,6 +88,13 @@ public class Song {
 		for (int i = b.length() - 1; i >= 0; i--)
 			tmp.append(b.charAt(i));
 		return tmp.toString();
+	}
+	
+	private boolean contains(String s, String a[]) {
+        for (int i = 0; i < a.length; i++)
+            if (s.equals(a[i]))
+                return true;
+        return false;
 	}
 	
 	private void seperatePath() {
@@ -91,6 +110,7 @@ public class Song {
 		}
 		s.deleteCharAt(0);
 		this.extension = reverse(tmp.toString());
+		this.origExt = this.extension;
 		
 		// Album
 		tmp = new StringBuilder();
@@ -99,7 +119,7 @@ public class Song {
 			s.deleteCharAt(0);
 		}
 		s.deleteCharAt(0);
-		this.title = this.extension = reverse(tmp.toString());
+		this.title = reverse(tmp.toString());
 		
 		// Artist
 		tmp = new StringBuilder();
@@ -108,7 +128,16 @@ public class Song {
 			s.deleteCharAt(0);
 		}
 		s.deleteCharAt(0);
-		this.title = this.extension = reverse(tmp.toString());
+		this.title = reverse(tmp.toString());
+		
+		// Parent
+		tmp = new StringBuilder();
+		while (s.charAt(0) != this.divider && s.length() > 0) {
+			tmp.append(s.charAt(0));
+			s.deleteCharAt(0);
+		}
+		s.deleteCharAt(0);
+		this.parent = reverse(tmp.toString());
 	}
 	
 	private String determineOutput(String outFolder) {
@@ -127,4 +156,54 @@ public class Song {
 		}
 		return s.toString();
 	}
+
+	/*
+        Public Methods
+    */
+    
+    
+    
+    /*
+        Data Access/Manipulation
+    */
+    public String getFullInputPath() { return this.fullInputPath; }
+    public String getFullOutputPath() { return this.fullOutputPath; }
+    public String getOutputFolder() { return this.outputFolder; }
+    public char getOutputMode() { return this.outputMode; }
+    public char getDivider() { return this.divider; }
+    public boolean willBeConverted() { return this.convertSong; }
+    public void setFullInputPath(String a, String outFolder, String ext, boolean noNumbers, char mode) throws IOException { 
+        this.fullInputPath = a;
+        this.outputFolder = outFolder;
+        this.seperatePath();
+        
+        // Determine folder divider char
+        if (this.fullInputPath.charAt(0) == '/')
+            this.divider = '/'; // Windows implicit, Linux/MacOS/BSD explicit. Conversion is later, so nbd
+        
+        // Check if extensions match allowed, or unconvetable extension types
+        if (this.extension == ext || contains(ext,allowedExt)) 
+            this.convertSong = false;
+        else if (contains(ext,unconvertable)) {
+            sLog.log("Song " + fullInputPath + " is unconvertable!",3);
+            this.convertSong = false;
+        }
+        else
+            this.extension = ext;
+        
+        // Delete numbers if user specified
+        if (noNumbers) {
+            StringBuilder s = new StringBuilder(this.title);
+            while (s.charAt(0) != ' ')
+                if (Character.isDigit(s.charAt(0)) || s.charAt(0) == '-')
+                    s.deleteCharAt(0);
+                else
+                    continue;
+        }
+        
+        this.outputMode = mode;
+        this.fullOutputPath = determineOutput(this.outputFolder);
+    }
+    public void swapDivider() { if (this.divider == '/') this.divider = '\\'; else this.divider = '/'; }
+    public void setConversion(boolean b) { this.convertSong = b; };
 }
